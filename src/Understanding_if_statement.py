dd250314f2035ab4655eebebd7ddee06c47a47d6
Gera Salazar <2530032@upv.edu.mx>
2025-11-14 08:48:34 -0600

    mas jale de listas :v

--- src/Understanding_if_statement.py
+++ src/Understanding_if_statement.py
@@ -26,3 +26,56 @@ requested_topping = "mushrooms"
 if requested_topping != "anchoves":
     print("Hold the anchovies")
 
+
+# Comparaciones numericas 
+age = 18
+print(age==18) # True
+
+answer = 17
+if answer != 42: # True
+    print("Esa no es la respuesta intenta de nuevo")
+
+
+age=19
+print(age<21) # True
+print(age<=21) # True
+print(age>21) # False
+print(age>=21) # False
+
+
+# Condicionales multiples 
+age_0 = 22
+age_1 = 18
+
+print(age_0>=21) # True
+print("and")
+# Operacion Logica and
+print(age_0>=21 and age_1>21) # False
+print(age_0>=21 and age_1>=18) # True
+
+
+age_0 = 22
+age_1 = 18
+print("or")
+# Operacion Logica or
+print(age_0>=22 or age_1>21) # True
+print(age_0>=23 or age_1>=21) # False
+
+print("elementos en una lista")
+# Preguntarme si un valor esta en una lista 
+cars = ["micro", "vocho", "tsuru", "tsubaro"]
+print("vocho" in cars) # True
+print("chevy" in cars) # False
+
+
+print("elementos q no estan en una lista")
+# Preguntar si un valor no esta en la lista 
+alumnos = ["victor", "ana", "maiki", "gera"]
+user = "josue"
+
+print(user not in alumnos) #True
+print("maiki" not in alumnos) # False
+
+# Datos booleanos 
+game_active = True
+can_edit = False
